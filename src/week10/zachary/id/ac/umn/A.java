8ae9aece77e1df29050c603bdd10b54fb5b6ded2
Cazhed <zachary.sechan@gmail.com>
2025-11-17 13:28:08 +0700

    Final Keyword

---
+++ src/week10/zachary/id/ac/umn/A.java
@@ -0,0 +1,8 @@
+package week10.zachary.id.ac.umn;
+
+public class A {
+
+	final void method1() {
+		System.out.println("This is a final method.");
+	}
+}
---
+++ src/week10/zachary/id/ac/umn/B.java
@@ -0,0 +1,8 @@
+package week10.zachary.id.ac.umn;
+
+public class B extends A {
+
+	void method1() {
+		System.out.println("Illegal!");
+	}
+}
